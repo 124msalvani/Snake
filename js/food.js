9ada2632458cee1663a7d7cb22061de15c877425
Margaret <124msalvani@frhsd.com>
2023-03-01 09:49:30 -0500

    Added eyes on snake, bg music, no wiggle ff

--- js/food.js
+++ js/food.js
@@ -1,6 +1,6 @@
 import { onSnake, expandSnake } from "./snake.js";
 import {randomGridPosition} from "./grid.js";
-let food = {x:10,y:1};
+let food = {x:11,y:5};
 const  EXPANSION_RATE = 1;
 var score = 1
 
@@ -19,7 +19,6 @@ export function draw(gameBoard) {
     foodElement.style.gridColumnStart = food.x
     foodElement.classList.add('food')
     gameBoard.appendChild(foodElement)
-    
 }
 
 function getRandomFoodPosition(){
--- js/script.js
+++ js/script.js
@@ -5,6 +5,9 @@ import {update as updateSnake, draw as drawSnake, SNAKE_SPEED, getSnakeHead, sna
 import {update as updateFood, draw as drawFood} from "./food.js";
 import { outsideGrid } from "./grid.js";
 
+var audio = document.getElementById("audio");
+audio.volume = 0.5;
+
 function main(currentTime){
     if (gameOver){
         if (confirm("You lost. Press ok to restart.")){
