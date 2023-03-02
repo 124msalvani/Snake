006943beaf7c855da65fd14a7494467e7cb9f6a3
Margaret <124msalvani@frhsd.com>
2023-03-02 10:55:45 -0500

    Sound effects, the color didn't work

--- js/food.js
+++ js/food.js
@@ -4,11 +4,16 @@ let food = {x:11,y:5};
 const  EXPANSION_RATE = 1;
 var score = 1
 
+
 export function update() {
     if (onSnake(food)){
         expandSnake(EXPANSION_RATE);
+        var audioEat = document.getElementById("eatAudio");
+        audioEat.volume = 0.1;
+        audioEat.play();
         food = getRandomFoodPosition();
         document.getElementById("scoreboard").innerHTML = score ++;
+        
     }
 }
 
